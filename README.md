b2620b3566d54e75f5afaa78484dc8f9cf01317b
adrn-mm <adrian.m.muhammad@gmail.com>
2024-05-21 13:19:50 +0700

    update readme

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 - [ ] Develop script scraping
 - [ ] Develop script cleansing
+
+# Task
+
+- Scrape link https://www.bi.go.id/id/statistik/ekonomi-keuangan/sekda/StatistikRegionalDetail.aspx?idprov=32 dengan ketentuan berikut:
+  - Bagian kategori : Kegiatan Perbankan
+  - Dengan kategori statistic no 4 dan 16 untuk setiap provinsi
+- Buat script untuk transform excelnya menjadi format tabel seperti berikut:![](https://i.imgur.com/sscTMxW.png)
--- scraping_download.py
+++ scraping_download.py
@@ -8,9 +8,10 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import subprocess
 from datetime import datetime
+from selenium.webdriver.support.ui import Select
 
 # define the url
-url= "https://www.bi.go.id/id/statistik/informasi-kurs/transaksi-bi/Default.aspx"
+url= "https://www.bi.go.id/id/statistik/ekonomi-keuangan/sekda/StatistikRegionalDetail.aspx?idprov=14"
 
 # Configure selenium via local driver
 options = webdriver.ChromeOptions()
@@ -20,4 +21,32 @@ driver = webdriver.Chrome(options=options)
 # print status
 print("Please wait, start scraping...")
 
+# solution for timeout
+driver.set_page_load_timeout(20)
+try:
+    driver.get(url)
+except TimeoutException:
+    driver.execute_script("window.stop();")
 
+# wait for elements to show up
+time.sleep(10)
+
+# Drop down menu for selecting province
+dropdown_provinsi = driver.find_element(By.ID, "DropDownListProvinsiSekda")
+select_provinsi = Select(dropdown_provinsi)
+select_provinsi.select_by_visible_text("Bengkulu")
+
+# Refresh the page
+driver.refresh()
+time.sleep(10)
+
+# Drop down menu for selecting category
+dropdown_category = driver.find_element(By.ID, "DropDownListCategorySekda")
+select_category = Select(dropdown_provinsi)
+select_category.select_by_visible_text("Kegiatan Perbankan")
+
+# Refresh the page
+driver.refresh()
+time.sleep(10)
+
+driver.quit()
