28148271d8cfe7dcbbebc6f185188d967a2d7e13
caveinjoe <99408687+caveinjoe@users.noreply.github.com>
2024-05-22 14:57:59 +0700

    Update scraping.py
    Added caching

--- scraping_download.py
+++ scraping_download.py
@@ -1,37 +1,29 @@
 # Import libraries
 import time
 import os
-import subprocess
+import requests
+import json
 import pandas as pd
-from datetime import datetime
+from bs4 import BeautifulSoup
+from io import BytesIO
+from zipfile import ZipFile
+from zipfile import ZipFile
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import Select
 
 
 # define the url
-url= "https://www.bi.go.id/id/statistik/ekonomi-keuangan/sekda/StatistikRegionalDetail.aspx"
+base_url= "https://www.bi.go.id/id/statistik/ekonomi-keuangan/sekda/StatistikRegionalDetail.aspx"
+
+# define cache link file
+link_file_path = './temp_links.json'
 
 # define category numbers to be scraped
 scrape_category_numbers = [4, 16]
-
-# Configure selenium via local driver
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(options=options)
-
-# print status
-print("Please wait, scraping in progress...")
-
-# solution for timeout
-driver.set_page_load_timeout(20)
-try:
-    driver.get(url)
-except TimeoutException:
-    driver.execute_script("window.stop();")
 
 # Progress bar
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -54,47 +46,176 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     # Print New Line on Complete
     if iteration == total: 
         print()
+
+def validate_datetime(file_path):
+    """
+    Returns True only if the file specified by 'filename' has been created within a specified time limit.
+    
+    Parameters:
+        filename (str): The path to the file to check for creation time.
         
-# Replaced sleep with wait to improve code efficiency
-try:
-    # Drop down menu for selecting province
-    # Wait until the dropdown element is present and clickable
-    def get_select(id):
-        dropdown = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, id))
-        )
-        select = Select(dropdown)
-        return select
+    Returns:
+        bool: True if the file has been created within the specified time limit, False otherwise.
+    """
+    if os.path.exists(file_path):
+        file_creation_time = os.path.getctime(file_path)
+        current_time = time.time()
+        # Check if the difference is greater than 24 hours (86400 seconds)
+        if current_time - file_creation_time < 86400:
+            return True
+    return False
 
-    select_provinsi = get_select('DropDownListProvinsiSekda')
-    total_options = len(select_provinsi.options)
-    printProgressBar(0, total_options, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    for i in range(total_options):
+def validate_link_file(file_path):
+    """
+    Returns True if the link cache file specified by 'filename' is valid for extraction.
+    
+    Parameters:
+        filename (str): The path to the link cache file to be validated.
+        
+    Returns:
+        bool: True if the link cache file is valid for extraction, False otherwise.
+    """
+    print("Validating cached links...")
+
+    # Check if the file exists
+    file_exists = os.path.exists(file_path)
+
+    # If the file exists and is not stale, load the contents
+    if file_exists and validate_datetime(file_path):
+        with open(file_path, 'r') as file:
+            try:
+                content = json.load(file)
+            except json.JSONDecodeError:
+                return False
+    else:
+        print("No compatible cache found...")
+        return False
+
+    # Check the links
+    if isinstance(content, dict):
+        for value in content.values():
+            if isinstance(value, list):
+                for item in value:
+                    if all([f'www.bi.go.id' in item, any([f'ii{num:02d}' in item for num in scrape_category_numbers])]):
+                        print("Extracting based on cached links...")
+                        print(json.dumps(content, indent=4))
+                        return True
+    print("Getting new links...")
+    return False
+
+def get_links(url):
+    """
+    Returns a dictionary containing the links to be used for extraction.
+    
+    Parameters:
+        url (str): The url of the web page to be scraped.
+        
+    Returns:
+        dict: A dictionary where each key represents a province and its corresponding value is a list of links.
+    """
+    # print status
+    print("Please wait, URL scraping in progress...")
+
+    # Configure selenium via local driver
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(options=options)
+
+    # solution for timeout
+    driver.set_page_load_timeout(20)
+    try:
+        driver.get(url)
+    except TimeoutException:
+        driver.execute_script("window.stop();")
+
+    # Output dict
+    category_links = dict()
+
+    # Replaced sleep with wait to improve code efficiency
+    try:
+        # Drop down menu for selecting province
+        # Wait until the dropdown element is present and clickable
+        def get_select(id):
+            dropdown = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, id))
+            )
+            select = Select(dropdown)
+            return select
+
         select_provinsi = get_select('DropDownListProvinsiSekda')
-        option = select_provinsi.options[i]
-        option_value = option.get_attribute('value')
-        province_name = option.get_attribute("text")
+        total_options = len(select_provinsi.options)
+        printProgressBar(0, total_options, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        for i in range(total_options):
+            # Re-initializing select to avoid staling
+            select_provinsi = get_select('DropDownListProvinsiSekda')
+            option = select_provinsi.options[i]
+            option_value = option.get_attribute('value')
+            province_name = option.get_attribute("text")
 
-        # Select the option
-        select_provinsi.select_by_value(option_value)
+            # Select the option
+            select_provinsi.select_by_value(option_value)
 
-        # Drop down menu for selecting category
-        select_category = get_select('DropDownListCategorySekda')
+            # Drop down menu for selecting category
+            select_category = get_select('DropDownListCategorySekda')
 
-        # Select the option
-        select_category.select_by_visible_text("Kegiatan Perbankan")
+            # Select the option
+            select_category.select_by_visible_text("Kegiatan Perbankan")
 
-        # Get XLS file
-        def get_cell_href(number):
-            cell_xpath = f'//*[@id="ctl00_ctl54_g_077c3f62_96a4_43aa_b013_8e274cf2ce9d_ctl00_divIsi"]/table/tbody/tr[{number}]/td[2]/a'
-            cell_element = driver.find_element(By.XPATH, cell_xpath)
-            return cell_element.get_attribute('href')
+            # Get XLS file links
+            def get_cell_href(number):
+                cell_xpath = f'//*[@id="ctl00_ctl54_g_077c3f62_96a4_43aa_b013_8e274cf2ce9d_ctl00_divIsi"]/table/tbody/tr[{number}]/td[2]/a'
+                cell_element = driver.find_element(By.XPATH, cell_xpath)
+                return cell_element.get_attribute('href')
 
-        category_details = [get_cell_href(number) for number in scrape_category_numbers]
+            link_dict = {province_name: [get_cell_href(number) for number in scrape_category_numbers]}
+            category_links.update(link_dict)
 
-        print(f"{province_name} URLS: {category_details}")   
-        printProgressBar(i, total_options, prefix = 'Progress:', suffix = 'Complete', length = 50)
+            print(f"{link_dict}")   
+            printProgressBar(i+1, total_options, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        
+    finally:
+        print(f"CATEGORY: {category_links}")
+        driver.quit()
+        with open(link_file_path, 'w') as link_file:
+            json.dump(category_links, link_file, indent=4)
+    
+    return category_links
 
-finally:
-    driver.quit()
+# Extract context (XLS files are actually in HTML format)
+def extract_context(category_links):
+    for url_list in category_links.values():
+        for url in url_list:
+            try:
+                print(f"Downloading: {url}")
+                response = requests.get(url)
+                response.raise_for_status()
+                with ZipFile(BytesIO(response.content)) as zipfile:
+                    # Extract each file in the ZIP archive
+                    for file_name in zipfile.namelist():
+                        with zipfile.open(file_name) as extracted_file:
+                            print(f"Extracting: {file_name}")
+                            soup = BeautifulSoup(extracted_file, 'html.parser')
+                            # Extracting the years from the relevant <td> tags
+                            year_row = soup.find('tr', class_='xl25')
+                            years = [td.get_text(strip=True) for td in year_row.find_all('td', class_='x128')]
+                            print(f"YEARS: {years}")
 
+
+            except requests.exceptions.RequestException as e:
+                print(f"HTTP request error: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+def main():
+    if not validate_link_file(link_file_path):
+        category_links = get_links(base_url)
+    else:
+        try:
+            with open(link_file_path, 'r') as file:
+                category_links = json.load(file)
+        except json.JSONDecodeError:
+            get_links(base_url)
+
+    extract_context(category_links)
+
+if __name__ == "__main__":
+    main()
